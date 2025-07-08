2e2b28c58efb6128e098f418e36c77187789478a
aiswaryapraveen <aiswaryapraveen2019@gmail.com>
2025-07-08 13:55:22 +0530

    Add image field to Note model

--- notes_project/settings.py
+++ notes_project/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 import os
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For collectstatic
 # Default primary key field type
